897ab510326e778db1b26150a48d35d7ff2de506
Sohaib-Ayub <sohaibayub20@gmail.com>
2023-10-25 10:43:15 +0500

    assignment 2 variables

---
+++ Assignment2/app.js
@@ -0,0 +1,19 @@
+let username = "Ali";
+      let myname = username + " Khan";
+
+      alert(username);
+      alert(myname);
+
+      let message = "Hello World";
+
+      alert(message);
+
+      let email = "myemail345";
+      alert(email + "@xyz.com");
+
+      let book = "A smarter way to learn javascript.";
+      alert("I am learning js from " + book);
+
+      document.write("I can write in HTML using js.\n");
+      document.write("My full name is " + myname);
+      document.write("My email is " + email);
