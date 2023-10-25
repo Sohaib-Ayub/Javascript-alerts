517a2e9ffbd0b553cb784d352932756e2bb2912a
Sohaib-Ayub <sohaibayub20@gmail.com>
2023-10-25 10:28:46 +0500

    alert JS assignment

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Error ! Please Enter a Valid Password.");
+      alert("Wellcome to Js Land\nHappy Coding.");
+
+      alert("Wellcome to Js Land\nHappy Coding.");
+      alert(
+        "Happy Coding.\nPrevent this page from creating additional dialogues"
+      );
+      alert('I can write this through JS on web console');
+      console.log('I can write this through JS on web console')
