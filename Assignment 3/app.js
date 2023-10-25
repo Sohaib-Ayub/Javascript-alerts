66ae07eb669bd7f76a8157798ac70f412165c20e
Sohaib-Ayub <sohaibayub20@gmail.com>
2023-10-25 10:56:19 +0500

    assignmet 3 variables

---
+++ Assignment 3/app.js
@@ -0,0 +1,22 @@
+let age = 23;
+      alert("I am " + age + " Year old. ");
+      let n = 15;
+      alert("You have visited the webpage " + n + " times");
+
+      let birthyear = 2000;
+
+      document.write("My birth year is " + birthyear);
+
+      let visitorname = "Salman";
+
+      let producttitle = "T-shirts";
+      let quantity = 15;
+      document.write(
+        " " +
+          visitorname +
+          " order " +
+          quantity +
+          " " +
+          producttitle +
+          " from xyz cloting store."
+      );
